13ca75e66115e1a9efa579d59d08e6d4ac8e14ab
Gabriel Scarcella <scarcella.ga@gmail.com>
2023-10-13 22:48:09 -0300

    refactors + admin + import csv (#3)
    * refactors + admin + import csv
    
    * fix pylint issues
    
    * add string representations
    
    * more pylint fixes

---
+++ app/api/import_csv/import_csv_actions.py
@@ -0,0 +1,102 @@
+"""Import CSV actions."""
+
+from typing import (
+    Iterable,
+    TypeVar,
+)
+import csv
+import logging
+
+from app import models
+
+
+logger = logging.getLogger(__name__)
+
+
+Readable = TypeVar("Readable", bound=Iterable[str])
+
+
+def import_csv_file(csv_file: Readable):
+    """
+    Import a csv file into the database for pet tags
+    :param csv_file: readable file IO object, or list/iterable of strings
+    :return:
+    """
+    logger.info("Importing csv file")
+    reader = csv.reader(csv_file, delimiter=",")
+    for idx, row in enumerate(reader):
+        if idx == 0:
+            # Skip the header row
+            continue
+        import_row(row)
+
+
+def import_row(row: list[str]):
+    """
+    Import a row from the csv file creating the database model for pettag
+    :param row: Row expect to have this format:
+        id,chapa,codigo,mascota,email,propietario,tel1,tel2,last_modified
+    :return:
+    """
+    # Extract the values from the row
+    _, tag_str, code, pet_name, email, owner_name, phone_str, alt_phone_str, _ = row
+    # Create empty placeholders
+    owner: models.Owner | None = None
+    pet: models.Pet | None = None
+    phone: models.Phone | None = None
+    alt_phone: models.Phone | None = None
+    # Get or create the tag
+    if not tag_str:
+        logger.info("Skipping empty tag")
+        return
+    logger.info("Get or create tag %s", tag_str)
+    tag, _ = models.PetTag.objects.get_or_create(
+        tag=tag_str,
+        defaults={
+            "code": code,
+        },
+    )
+    # Prepare Phones if they exist
+    if phone_str:
+        logger.info("Get or create phone %s", phone_str)
+        phone, _ = models.Phone.objects.get_or_create(
+            phone=phone_str,
+        )
+        if phone not in tag.phones.all():
+            logger.info("Add phone %s to tag %s", phone_str, tag_str)
+            tag.phones.add(phone)
+    if alt_phone_str:
+        logger.info("Get or create alt phone %s", alt_phone_str)
+        alt_phone, _ = models.Phone.objects.get_or_create(
+            phone=alt_phone_str,
+        )
+        if alt_phone not in tag.phones.all():
+            logger.info("Add alt phone %s to tag %s", alt_phone_str, tag_str)
+            tag.phones.add(alt_phone)
+    # Get or create the owner
+    if owner_name:
+        logger.info("Get or create owner %s", owner_name)
+        owner, _ = models.Owner.objects.get_or_create(
+            name=owner_name,
+            defaults={
+                "email": email,
+            },
+        )
+        if phone and phone not in owner.phones.all():
+            logger.info("Add phone %s to owner %s", phone_str, owner_name)
+            owner.phones.add(phone)
+        if alt_phone and alt_phone not in owner.phones.all():
+            logger.info("Add alt phone %s to owner %s", alt_phone_str, owner_name)
+            owner.phones.add(alt_phone)
+        tag.owner = owner
+    # create the pet if present
+    if pet_name:
+        logger.info("Creating Pet %s", pet_name)
+        pet = models.Pet.objects.create(
+            name=pet_name,
+            owner=owner,
+        )
+        tag.pet = pet
+    # Save the tag
+    logger.info("Saving tag %s", tag_str)
+    tag.save()
